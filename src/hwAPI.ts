1a005a987bdd8b72f039e8a6dbc61bb902a8a4a8
cptechie <cptechie@users.noreply.github.com>
2020-09-30 23:15:16 -0500

    Testing serial port

--- src/hwAPI.ts
+++ src/hwAPI.ts
@@ -9,7 +9,27 @@ export class HWAPI {
     }
 
     get_devices(){
-        let results = ['01:04:01:05:03']
+        const SerialPort = require('serialport')
+        const Readline = require('@serialport/parser-readline')
+        const port = new SerialPort(this.config.serialPath, { 
+            baudRate: 115200,
+            parser: SerialPort.parsers.readline("\n")
+        })
+
+        const parser = new Readline()
+        port.pipe(parser)
+
+        let results = []
+
+        parser.on('data', function(line){
+            if ( line.includes('RDL, ') ) {
+                results.push(line);
+            }
+        });
+
+        port.write('RDL, [01:04:01:05:03]\n');
+
+        // let results = ['01:04:01:05:03']
 
         return results
     }
